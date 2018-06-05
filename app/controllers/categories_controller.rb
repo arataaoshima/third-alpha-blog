342fb529ce56c5c713d9e9f4d3697b215afe1c0e
=Arata <=newbluelight1031@gmail.com>
2018-06-05 22:51:50 +0000

    edit category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -21,9 +21,27 @@ class CategoriesController < ApplicationController
     
   end
   
-  def show
+  def edit
+    @category = Category.find(params[:id])
   end
+  
+  def update
+     @category = Category.find(params[:id])
+     if @category.update(category_params)
+       flash[:success] = "Category name was successfully updated"
+       redirect_to category_path(@category)
+     else
+     end
+  end
+  
 
+def show
+
+@category = Category.find(params[:id])
+
+@category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
+
+end
 private
   def category_params
     params.require(:category).permit(:name)
